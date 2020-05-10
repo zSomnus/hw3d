c389e68b2b5336d03426ff2db4c46d87b509e093
Ella Zhang <ellazhang37@gmail.com>
2020-05-10 18:30:23 -0400

    + Add WindowsMessageMap

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,7 +1,11 @@
 #include <Windows.h>
+#include "WindowsMessageMap.h"
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+
 	switch (msg)
 	{
 	case WM_CLOSE:
@@ -17,7 +21,7 @@ int CALLBACK WinMain(
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	const auto pClassName = L"hw3d2333";
+	const auto pClassName = "hw3d2333";
 	// register window class
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
@@ -36,7 +40,7 @@ int CALLBACK WinMain(
 	HWND hWnd = CreateWindowEx(
 		0,
 		pClassName,
-		L"Happy Hard Window",
+		"Happy Hard Window",
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
 		200, 200, 640, 480,
 		nullptr,
