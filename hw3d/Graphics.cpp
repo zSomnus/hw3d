61a16a26ba1cf301b8359d0e488818ab39096576
Ella Zhang <ellazhang37@gmail.com>
2020-06-13 23:34:04 -0400

    + Constant Buffers

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -100,9 +100,8 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 	pContext->ClearRenderTargetView(pTarget.Get(), color);
 }
 
-void Graphics::DrawTestTriangle()
+void Graphics::DrawTestTriangle(float angle)
 {
-	namespace wrl = Microsoft::WRL;
 	HRESULT hr;
 
 	struct Vertex
@@ -129,7 +128,7 @@ void Graphics::DrawTestTriangle()
 		{ -0.5f, -0.5f, 0,0,255,0 },
 		{ -0.3f, 0.3f, 0, 255, 0, 0 },
 		{ 0.3f, 0.3f, 0, 0, 255, 0 },
-		{ 0.0f, -0.8f, 255, 0, 0, 0 },
+		{ 0.0f, -1.0f, 255, 0, 0, 0 },
 	};
 	vertices[0].color.g = 255;
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
@@ -172,6 +171,42 @@ void Graphics::DrawTestTriangle()
 
 	// Bind index buffer to pipeline
 	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
+
+	// Create constant buffer for transformation matrix
+	struct ConstantBuffer
+	{
+		struct
+		{
+			float element[4][4];
+		} transformation;
+	};
+	const ConstantBuffer cb =
+	{
+		{
+			(3.0f / 4.0f) *  std::cos(angle), std::sin(angle), 0.0f, 0.0f,
+			(3.0f / 4.0f) * -std::sin(angle), std::cos(angle), 0.0f, 0.0f,
+			0.0f			, 0.0f			 , 1.0f, 0.0f,
+			0.0f			, 0.0f			 , 0.0f, 1.0f,
+		}
+	};
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer;
+	D3D11_BUFFER_DESC cbd = {};
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	/** Creating as dynamic allow the use of the Lock function.
+		It's also possible to update with UpdateSubresource on
+		a non-dynamic.
+	*/
+	cbd.Usage = D3D11_USAGE_DYNAMIC;
+	cbd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cbd.MiscFlags = 0u;
+	cbd.ByteWidth = sizeof(cb);
+	cbd.StructureByteStride = 0u;
+	D3D11_SUBRESOURCE_DATA csd = {};
+	csd.pSysMem = &cb;
+	GFX_THROW_INFO(pDevice->CreateBuffer(&cbd, &csd, &pConstantBuffer));
+
+	// Bind constant buffer to vertex shader
+	pContext->VSSetConstantBuffers(0u, 1u, pConstantBuffer.GetAddressOf());
 
 	// Create pixel shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
--- hw3d/VertexShader.hlsl
+++ hw3d/VertexShader.hlsl
@@ -4,10 +4,18 @@ struct VSOut
 	float4 pos : SV_POSITION;
 };
 
+cbuffer CBuf
+{
+	// The keyword matrix indicates 4x4 float
+	// row_major is slower than using a transposed matrix
+	// The inverse of a rotation matrix is its transpose
+	row_major matrix transform;
+};
+
 VSOut main(float4 pos : POSITION, float4 color : COLOR)
 {
 	VSOut vso;
+	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
 	vso.color = color;
-	vso.pos = float4(pos.x, pos.y, 0.0f, 1.0f);
 	return vso;
 }
